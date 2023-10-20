b1f03e660da20b89bde039664a34eae7fcbb5be8
jora singh <jora>
2023-10-20 15:38:58 +1100

    instructions for this week

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JOmni.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JOmni.java
@@ -1,32 +1,3 @@
-/* Copyright (c) 2021 FIRST. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided that
- * the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice, this list
- * of conditions and the following disclaimer.
- *
- * Redistributions in binary form must reproduce the above copyright notice, this
- * list of conditions and the following disclaimer in the documentation and/or
- * other materials provided with the distribution.
- *
- * Neither the name of FIRST nor the names of its contributors may be used to endorse or
- * promote products derived from this software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
- * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
- * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
@@ -63,10 +34,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Jomni", group="Linear OpMode")
+@TeleOp(name = "Basic: Jomni", group = "Linear OpMode")
 public class JOmni extends LinearOpMode {
 
-    // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFrontDrive = null;
     private DcMotor leftBackDrive = null;
@@ -77,30 +47,21 @@ public class JOmni extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        // Initialize the hardware variables. Note that the strings used here must correspond
-        // to the names assigned during the robot configuration step on the DS or RC devices.
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        // Initialize the hardware variables. Note that the strings used here must
+        // correspond
+        // to the names assigned during the robot configuration step on the DS or RC
+        // devices.
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
         rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
         rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
         linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
 
-        // ########################################################################################
-        // !!!            IMPORTANT Drive Information. Test your motor directions.            !!!!!
-        // ########################################################################################
-        // Most robots need the motors on one side to be reversed to drive forward.
-        // The motor reversals shown here are for a "direct drive" robot (the wheels turn the same direction as the motor shaft)
-        // If your robot has additional gear reductions or uses a right-angled drive, it's important to ensure
-        // that your motors are turning in the correct direction.  So, start out with the reversals here, BUT
-        // when you first test your robot, push the left joystick forward and observe the direction the wheels turn.
-        // Reverse the direction (flip FORWARD <-> REVERSE ) of any wheel that runs backward
-        // Keep testing until ALL the wheels move the robot forward when you push the left joystick forward.
         leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
 
-        // Wait for the game to start (driver presses PLAY)
         telemetry.addData("Status", "Initialized");
         telemetry.update();
 
@@ -111,17 +72,19 @@ public class JOmni extends LinearOpMode {
         while (opModeIsActive()) {
             double max;
 
-            // POV Mode uses left joystick to go forward & strafe, and right joystick to rotate.
-            double axial   = -gamepad1.left_stick_y;  // Note: pushing stick forward gives negative value
-            double lateral =  gamepad1.left_stick_x;
-            double yaw     =  gamepad1.right_stick_x;
+            // POV Mode uses left joystick to go forward & strafe, and right joystick to
+            // rotate.
+            double axial = -gamepad1.left_stick_y; // Note: pushing stick forward gives negative value
+            double lateral = gamepad1.left_stick_x;
+            double yaw = gamepad1.right_stick_x;
 
-            // Combine the joystick requests for each axis-motion to determine each wheel's power.
+            // Combine the joystick requests for each axis-motion to determine each wheel's
+            // power.
             // Set up a variable for each drive wheel to save the power level for telemetry.
-            double leftFrontPower  = axial + lateral + yaw;
+            double leftFrontPower = axial + lateral + yaw;
             double rightFrontPower = axial - lateral - yaw;
-            double leftBackPower   = axial - lateral + yaw;
-            double rightBackPower  = axial + lateral - yaw;
+            double leftBackPower = axial - lateral + yaw;
+            double rightBackPower = axial + lateral - yaw;
 
             // Normalize the values so no wheel power exceeds 100%
             // This ensures that the robot maintains the desired motion.
@@ -130,38 +93,16 @@ public class JOmni extends LinearOpMode {
             max = Math.max(max, Math.abs(rightBackPower));
 
             if (max > 1.0) {
-                leftFrontPower  /= max;
+                leftFrontPower /= max;
                 rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+                leftBackPower /= max;
+                rightBackPower /= max;
             }
 
+            // TODO
             // Control the linear slide motor using gamepad triggers
-            double linearSlidePower = 0.0;
-            if (gamepad1.right_trigger > 0) {
-                linearSlidePower = 0.2; // Adjust the power as needed
-            } else if (gamepad1.left_trigger > 0) {
-                linearSlidePower = -0.2; // Adjust the power as needed
-            }
+            //HINT gamepad1.right_trigger gamepad1.left_trigger
 
-            // This is test code:
-            //
-            // Uncomment the following code to test your motor directions.
-            // Each button should make the corresponding motor run FORWARD.
-            //   1) First get all the motors to take to correct positions on the robot
-            //      by adjusting your Robot Configuration if necessary.
-            //   2) Then make sure they run in the correct direction by modifying the
-            //      the setDirection() calls above.
-            // Once the correct motors move in the correct direction re-comment this code.
-
-            /*
-            leftFrontPower  = gamepad1.x ? 1.0 : 0.0;  // X gamepad
-            leftBackPower   = gamepad1.a ? 1.0 : 0.0;  // A gamepad
-            rightFrontPower = gamepad1.y ? 1.0 : 0.0;  // Y gamepad
-            rightBackPower  = gamepad1.b ? 1.0 : 0.0;  // B gamepad
-            */
-
-            // Send calculated power to wheels
             leftFrontDrive.setPower(leftFrontPower);
             rightFrontDrive.setPower(rightFrontPower);
             leftBackDrive.setPower(leftBackPower);
@@ -171,7 +112,8 @@ public class JOmni extends LinearOpMode {
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("Front left/Right", "%4.2f, %4.2f", leftFrontPower, rightFrontPower);
             telemetry.addData("Back left/Right", "%4.2f, %4.2f", leftBackPower, rightBackPower);
-            telemetry.addData("Linear Slide Power", "%4.2f", linearSlidePower);
+            telemetry.addData("Trigger Values", "%4.2f, %4.2f", gamepad1.right_trigger, gamepad1.left_trigger);
             telemetry.update();
         }
-    }}
+    }
+}
--- README.md
+++ README.md
@@ -1,8 +1,104 @@
-# FTC
-This is the repo for the team WeLoveStem. We will have all our documents and code posted here.
+# FTC 2023 - Centerstage
 
-Originally created by Bhargav and Kianna 2023
- oh golly gosh
- heloooo
- sifjiowajfioeajfio
- asdf
+## Diary
+### Week 1 Term 4
+Kiana and Bhargav this is what we need to do this week! - Jora
+
+#### GIT
+I've added a working base for out teamCode, but first we need to pull all the changes I made to your local PC(s). Today you can either choose to work together or seperately.
+
+Lets start.
+First lets open up terminal/powershell and navigate to our FTC folder. If your folder is in Documents follow the example below.
+
+```
+
+> cd Documents/FTC
+
+```
+ If your having trouble finding your folder through the terminal. Delete the copy of FTC you currently have and enter these commands into terminal/powershell. NOTE: the '>' is not part of the command but indicates that its a new line in the terminal.
+
+ ```
+ > cd ~
+ > cd Documents
+ > git clone git@github.com:JorawarSingh09/FTC.git
+ ```
+
+ This wil download the latest version of the repo to your computer. From now on if you need to download the repo  again you will need to "cd" into the "FTC" folder and use the command "git pull".
+ ```
+ > cd ~
+ > cd Documents\FTC
+ > git pull
+ ```
+
+ ### Code
+ Open up the folder FtcRobotControllerMaster Controller in Android studio. 
+
+ Open up the file  "JOmni.java" which can be found in TeamCode\src\java.
+ This is the current Teleop Code that we're working with. We talked about Java in the holiday session so I'm going to give a short explanation of what everythign does.
+
+ ```
+ @TeleOp(name="Basic: Jomni", group="Linear OpMode")
+ ```
+ This is what shows up in our driver station and set the name and mode of the code.
+
+ ```
+ public class JOmni extends LinearOpMode {}
+ ```
+ This creates the JOmni class, which is a child of the LinearOpMode class and thus inherits all of LinearOpModes features, such as objects contained within, quickly look the LinearOpMode class and discern the differences.
+
+ ```
+    private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftFrontDrive = null;
+    private DcMotor leftBackDrive = null;
+    private DcMotor rightFrontDrive = null;
+    private DcMotor rightBackDrive = null;
+    private DcMotor linearSlide = null;
+ ```
+Here we are creating a bunch of different objects, mainly for our motors. We are also creating an object for Elapsed time. we can use this for operations that require time based actions. Time is the only object that isnt empty, since it is initialised using the new ElapsedTime constructor. 
+
+```
+public void runOpMode()
+```
+This is the method that gets called by the driver station when we hit play. Everything in this method (or function is another name) is run on start.
+
+```
+leftFrontDrive  = hardwareMap.get(DcMotor.class, "left_front_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        linearSlide = hardwareMap.get(DcMotor.class, "linear_slide");
+```
+This section gets the motor mapping, or where we have plugged in our motors and other hardware in the robot. This information is taken from the hardwareMap, which is the configuration info we set in the driverstation.
+
+### Task 1: Comment out OMniCode
+To get a goood understanding of the code I want you comment as much of the code you can and use your own words to describe that the function of the code is. For all Objects in the code I want you to write out what the object type is and how its initialised if at all.
+
+Once this is done send the code to the Robot. COnnect to the control hub using usb c and you should see the control hub pop up in android studio in the top bar next to the play sign. Press the play and wait for the code to be pushed.
+
+### Task 2: Code for linear slides
+You may Have noticed a TODO notice in your Code. I need you guys to work together and make the code for the linear slides. Look to the code that makes the drivetrain motors to work for inspiration.
+The Requirements will be:
+- Left trigger on the controller will make the slides do DOWN
+- Right trigger will make the slides go up
+- MAKE SURE THIS IS SLOW, we dont have any hard limits yet so chill.
+
+What you basically need to do is SET the POWER of the linearSlides motor to the value of the triggers. You may need to scale this value in order to restrict power, you can chcek what values the triggers output in the Telemetry menu in the DriverStation (Bhargav Phone). Just run it and play around with the controller to see what the min and max values are. 
+
+
+### Task 3: What do we need?
+#### Bhargav
+Talk to the mech team about linear slides. How do they need it to function? do they need it to stop at certain levels. Move at a certain speed. Ask them everything you can and write the QA below (ReadMe File)
+Q:
+A:
+
+Q:
+A:
+
+#### Kianna
+Talk to the mech team about the grabbing mech. How do they need it to function? How should the servo work> DO they want set movement or adjustable movement. Ask them everything you can and write the QA below (ReadMe File)
+
+Q:
+A:
+
+Q:
+A:
