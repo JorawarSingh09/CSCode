307bb7ad91077a52969397c3026e20276e9d5a97
Kianna <nunya@business.com>
2023-12-10 09:45:57 +1100

    Fixed auto for the most part, need to make adjustments to park
    need to park

--- FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveCalibrate.java
+++ FtcRobotController-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/DriveCalibrate.java
@@ -6,17 +6,20 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.CenterStageRobot;
+import org.firstinspires.ftc.teamcode.autonomous.enums.ParkingMode;
 
-@Disabled
 @Autonomous(name = "USED FOR TESTING ONLY", group = "Linear OpMode")
 public class DriveCalibrate extends LinearOpMode {
     CenterStageRobot myRobot;
+
+    Movements myMovements;
     Gamepad driver;
     Gamepad operator;
 
     @Override
     public void runOpMode() throws InterruptedException {
         myRobot = new CenterStageRobot(hardwareMap, telemetry);
+        myMovements = new Movements(myRobot);
 
         myRobot.startPosition();
 
@@ -24,21 +27,9 @@ public class DriveCalibrate extends LinearOpMode {
         telemetry.update();
         waitForStart();
         boolean canRun = true;
-
+        myMovements.park(ParkingMode.REDCLOSE);
         while(opModeIsActive() && canRun){
-            myRobot.driveForward();
-            sleep(1300);
-            myRobot.turnLeft();
-            sleep(800);
-            myRobot.driveForward();
-            sleep(150);
-            myRobot.driveStop();
-            myRobot.pickupPosition();
-            myRobot.driveBack();
-            sleep(450);
-            myRobot.driveStop();
-            myRobot.closeClaw();
-            myRobot.drivePosition();
+
 
 
 
